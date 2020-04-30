093c43fb932b77f274274e65b61be00575ab6f39
tooobening <48101049+tooobening@users.noreply.github.com>
2020-04-30 01:23:03 -0500

    add clustering results as a new column in csv file

--- js/main.js
+++ js/main.js
@@ -9,18 +9,28 @@
 // var options = {units: 'miles', maxEdge: 1};
   
 // var hull = turf.concave(points, options);
-//add OSM base tilelayer
-mymap = L.map('mapid').setView([50,-100],4)
+var mymap;
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/dark-v10', //https://docs.mapbox.com/api/maps/#mapbox-styles
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoieXVuaW5nbGl1IiwiYSI6ImNrNm9tNDFhcDBpejgzZG1sdnJuaTZ4MzYifQ.qYhM3_wrbL6lyTTccNKx_g' //'your.mapbox.access.token'
-}).addTo(mymap);
+//function to instantiate the Leaflet map
+function createMap(){
+
+    //add OSM base tilelayer
+    mymap = L.map('mapid').setView([40,-100],4)
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11', //https://docs.mapbox.com/api/maps/#mapbox-styles
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoieXVuaW5nbGl1IiwiYSI6ImNrNm9tNDFhcDBpejgzZG1sdnJuaTZ4MzYifQ.qYhM3_wrbL6lyTTccNKx_g' //'your.mapbox.access.token'
+    }).addTo(mymap);
+
+    //call getData function
+    // getData();
+};
 
 //Execute
-// $(document).ready(createMap);
+console.log('34')
+$(document).ready(createMap);
 // L.geoJson(hull).addTo(mymap);
