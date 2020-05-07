66eab5c771aa840fe53ccd4e63a49181a57fb9dd
wshujin <54903336+wshujin@users.noreply.github.com>
2020-05-07 08:37:46 -0600

    filter

--- js/main.js
+++ js/main.js
@@ -437,3 +437,266 @@ d3.csv("data/data-para-ca.csv", function(data) {
       .style("fill","white")
   d3.select(".myButton").on('click',resetHighlight);
 })
+
+
+//----------Filter: Bar Chart----------
+//create dataset for month count
+var monthcount = [{"month": "Jan", "count": 0},
+                  {"month": "Feb", "count": 0},
+                  {"month": "Mar", "count": 0},
+                  {"month": "Apr", "count": 0},
+                  {"month": "May", "count": 0},
+                  {"month": "Jun", "count": 0},
+                  {"month": "Jul", "count": 0},
+                  {"month": "Aug", "count": 0},
+                  {"month": "Sep", "count": 0},
+                  {"month": "Oct", "count": 0},
+                  {"month": "Nov", "count": 0},
+                  {"month": "Dec", "count": 0}];
+for (i=0; i<northern["features"].length; i++){
+  month = northern["features"][i]["properties"]["month"]
+  for (j=0; j<monthcount.length; j++){
+    if (month==monthcount[j]["month"]){
+      monthcount[j]["count"] = monthcount[j]["count"]+1;
+    }
+  }
+}
+
+//create bar chart for month count
+var chartWidth = 400;
+var chartHeight = 300;
+var chart = d3.select("body")
+    .append("svg")
+    .attr("width", chartWidth)
+    .attr("height", chartHeight)
+    .attr("class", "chart");
+var bars = chart.selectAll(".bars")
+    .data(monthcount)
+    .enter()
+    .append("rect")
+    .attr("class", function(d){
+        return "bars " + d.month;
+    })
+    .on("mouseover", highlight)
+    .on("mouseout", dehighlight)
+    .on("click", turnoff);
+var desc = bars.append("desc")
+    .text('{"stroke": "none", "stroke-width": "0px"}');
+var numbers = chart.selectAll(".numbers")
+    .data(monthcount)
+    .enter()
+    .append("text")
+    .attr("class", "numbers");
+var labels = chart.selectAll(".labels")
+    .data(monthcount)
+    .enter()
+    .append("text")
+    .attr("class", "labels");
+
+//draw bars based on month count data
+var n = monthcount.length;
+var yScale = d3.scaleLinear()
+    .range([0, chartHeight])
+    .domain([0, 500]);
+bars.attr("width", chartWidth/n - 10)
+    .attr("x", function(d, i){
+        return i * (chartWidth/n);
+    })
+    .attr("height", function(d){
+        return yScale(d.count);
+    })
+    .attr("y", function(d){
+        return chartHeight - yScale(d.count);
+    })
+    .style("fill", "yellow");
+numbers.attr("text-anchor", "middle")
+    .attr("x", function(d, i){
+        var fraction = chartWidth/n;
+        return i * fraction + (fraction-10)/2;
+    })
+    .attr("y", function(d){
+        return chartHeight - yScale(d.count) + 15;
+    })
+    .text(function(d){
+        return d.count;
+    });
+labels.attr("text-anchor", "middle")
+    .attr("x", function(d, i){
+        var fraction = chartWidth/n;
+        return i * fraction + (fraction-10)/2;
+    })
+    .attr("y", function(d){
+        return chartHeight - yScale(d.count) - 5;
+    })
+    .text(function(d){
+        return d.month;
+    });
+
+//create hover events
+function highlight(props){
+    var selected = d3.select(this)
+        .style("stroke", "white")
+        .style("stroke-width", "2");
+};
+function dehighlight(props){
+    var selected = d3.select(this)
+        .style("stroke", function(){
+            return getStyle(this, "stroke")
+        })
+        .style("stroke-width", function(){
+            return getStyle(this, "stroke-width")
+        });
+    function getStyle(element, styleName){
+        var styleText = d3.select(element)
+            .select("desc")
+            .text();
+        var styleObject = JSON.parse(styleText);
+        return styleObject[styleName];
+    };
+};
+
+//create click events
+var pointon;
+function turnoff(props){
+    if (!d3.select(this).classed("selected")){
+        d3.select(this)
+            .style('fill', 'grey')
+            .classed("selected", true)
+    } else {
+        d3.select(this)
+            .style('fill', 'yellow')
+            .classed("selected", false);
+    }
+
+    //create filter
+    notenable = {}
+    offmonth = document.getElementsByClassName("selected")
+    for (i=0; i<offmonth.length; i++){
+        notenable[offmonth[i].classList[1]] = false;
+    }
+    console.log(notenable);
+    mymap.removeLayer(start);
+    if (pointon){
+        mymap.removeLayer(pointon);
+    }
+    pointon = L.geoJson(northern, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.month);
+        },
+        filter: function(feature, layer){
+            return !(feature.properties.month in notenable);
+        },
+        pointToLayer: function(feature, latlng){
+            return L.circleMarker(latlng, stylePoint(feature));
+        },
+        onEachFeature: onEachFeature
+    }).addTo(mymap);
+};
+
+//----------Filter: Pie Chart----------
+//create dataset for color count
+var colorcount = [{"color": "yellow", "count": 0},
+                  {"color": "green", "count": 0},
+                  {"color": "blue", "count": 0},
+                  {"color": "purple", "count": 0},
+                  {"color": "red", "count": 0},
+                  {"color": "white", "count": 0}];
+for (i=0; i<northern["features"].length; i++){
+  color = northern["features"][i]["properties"]["color_new"]
+  for (j=0; j<colorcount.length; j++){
+    if (color==colorcount[j]["color"]){
+      colorcount[j]["count"] = colorcount[j]["count"]+1;
+    }
+  }
+}
+var data = {}
+data[colorcount[0].color] = colorcount[0].count
+data[colorcount[1].color] = colorcount[1].count
+data[colorcount[2].color] = colorcount[2].count
+data[colorcount[3].color] = colorcount[3].count
+data[colorcount[4].color] = colorcount[4].count
+data[colorcount[5].color] = colorcount[5].count
+
+//create pie chart for color count
+var pieWidth = 200;
+var pieHeight = 200;
+var radius = Math.min(pieWidth, pieHeight)/2;
+var colorScale = d3.scaleOrdinal()
+    .domain(data)
+    .range(['#c8ed03','#5fb676','#142b5c','#9879a0','#992d22','#f5f4f4'])
+var pie = d3.select("body")
+    .append("svg")
+    .attr("width", pieWidth)
+    .attr("height", pieHeight)
+    .attr("class", "pie")
+    .append("g")
+    .attr("transform", "translate(" + pieWidth/2 + "," + pieHeight/2 + ")");
+var piepie = d3.pie()
+    .value(function(d) {return d.value; })
+var data_ready = piepie(d3.entries(data))
+var rings = pie.selectAll('.rings')
+    .data(data_ready)
+    .enter()
+    .append('path')
+    .attr('d', d3.arc()
+        .innerRadius(50)
+        .outerRadius(radius)
+    )
+    .attr('fill', function(d) {return(colorScale(d.data.key)) })
+    .attr("stroke", "black")
+    .style("stroke-width", "2px")
+    .attr("class", function(d){
+        return "rings " + d.data.key;
+    })
+    .on("mouseover", highlight)
+    .on("mouseout", dehighlight)
+    .on("click", turnoff2);
+var desc = rings.append("desc")
+    .text('{"stroke": "black", "stroke-width": "2px"}');
+
+//create click events
+var pointon;
+function turnoff2(props){
+    if (!d3.select(this).classed("selected")){
+        d3.select(this)
+            .style('fill', 'grey')
+            .classed("selected", true)
+    } else {
+        d3.select(this)
+            .style('fill', function(){
+                return getStyle(this, "fill")
+            })
+            .classed("selected", false);
+        function getStyle(element, styleName){
+            var styleText = d3.select(element)
+                .select("desc")
+                .text();
+            var styleObject = JSON.parse(styleText);
+            return styleObject[styleName];
+        };
+    }
+
+    //create filter
+    notenable = {}
+    offcolor = document.getElementsByClassName("selected")
+    for (i=0; i<offcolor.length; i++){
+        notenable[offcolor[i].classList[1]] = false;
+    }
+    console.log(notenable);
+    mymap.removeLayer(start);
+    if (pointon){
+        mymap.removeLayer(pointon);
+    }
+    pointon = L.geoJson(northern, {
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.month);
+        },
+        filter: function(feature, layer){
+            return !(feature.properties.color_new in notenable);
+        },
+        pointToLayer: function(feature, latlng){
+            return L.circleMarker(latlng, stylePoint(feature));
+        },
+        onEachFeature: onEachFeature
+    }).addTo(mymap);
+};
